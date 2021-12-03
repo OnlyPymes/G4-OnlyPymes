1e67c4e1ef701dc5600b5133456fe6edeb2fc6e8
Sigmastr <cristianmonttfierro@gmail.com>
2021-12-03 16:07:49 -0300

    se agrega validaciones

--- register.php
+++ register.php
@@ -15,6 +15,7 @@ Include('conexion.php');
   <script src="https://kit.fontawesome.com/be8c81ece6.js" crossorigin="anonymous"></script>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+  
 </head>
 
 <body>
@@ -27,7 +28,7 @@ Include('conexion.php');
       <div class="mb-3 mt-3">
         <label for="usr" class="form-label">Rut:</label>
         <div class="input-contenedor">
-          <input name="rut" type="text" class="form-control" id="usr" placeholder="Ingrese el rut de su pyme">
+          <input name="rut" type="text" class="form-control" id="usuario" placeholder="Ingrese el rut de su pyme">
         </div>
 
       </div>
@@ -97,12 +98,12 @@ Include('conexion.php');
 
       </div>
 
-      <button type="submit" class="btn btn-primary" onsubmit="return validarFormulario();">Registrarse</button>
+      <button type="submit" class="btn btn-primary"   >Registrarse</button>
     </form>
   </div>
 
 
 </body>
-<script src="validacionRegister.js"></script>
 <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+<script src="validacionesRegister.js"></script>
 </html>
